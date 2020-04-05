b752147992144b91f06e67c1f0ddc3340dbf604d
Tristan Tarpley <tristan@marketr.life>
2020-04-05 18:25:57 -0500

    testing integration w/ wix and github

--- list.js
+++ list.js
@@ -1,3 +1,28 @@
-export default function test () {
-    console.log('called')
-}
+
+const test = () => {
+    let p = document.createElement('p')
+    p.textContent = 'hello'
+  
+    return p
+  }
+  export default class AppsList extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    static get observedAttributes() {
+      return ['data'];
+    }
+  
+    attributeChangedCallback(value, old, new_val) {
+      console.log('called')
+      console.log(JSON.parse(new_val))
+    }
+  
+    connectedCallback() {
+      this.appendChild(test())
+    }
+  }
+  
+customElements.define('apps-list', AppsList);
+  
